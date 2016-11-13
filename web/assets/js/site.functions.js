08648a5c1788af148f7d239081da92c5ab50806c
Manuel Kaufmann <humitos@gmail.com>
2016-11-13 16:09:00 -0500

    Untabify

--- web/assets/js/site.functions.js
+++ web/assets/js/site.functions.js
@@ -1,9 +1,9 @@
 function get_poi(element) {
     if ($('#expert-mode').is(':checked'))
-	return {
-	    name: 'Custom Query',
-	    iconName: 'notvisited'
-	}
+        return {
+            name: 'Custom Query',
+            iconName: 'notvisited'
+        }
 
     // TODO: improve this
     var type = ''
@@ -18,11 +18,11 @@ function get_poi(element) {
         if (type == '') type = e.tags.tourism;
     }
     if (e.tags.shop) {
-	if (e.tags.car_repair == 'wheel_repair') type = 'wheel_repair';
-	if (type == '') type = e.tags.shop;
+        if (e.tags.car_repair == 'wheel_repair') type = 'wheel_repair';
+        if (type == '') type = e.tags.shop;
     }
     if (e.tags.leisure) {
-	if (type == '') type = e.tags.leisure;
+        if (type == '') type = e.tags.leisure;
     }
 
     var poi = pois[type];
@@ -36,73 +36,73 @@ function callback(data) {
     if (spinner == 0) $('#spinner').hide();
 
     for(i=0; i < data.elements.length; i++) {
-	e = data.elements[i];
+        e = data.elements[i];
 
-	if (e.id in this.instance._ids) return;
-	this.instance._ids[e.id] = true;
+        if (e.id in this.instance._ids) return;
+        this.instance._ids[e.id] = true;
 
-	var pos = (e.type == 'node') ?
-	    new L.LatLng(e.lat, e.lon) :
-	    new L.LatLng(e.center.lat, e.center.lon);
+        var pos = (e.type == 'node') ?
+            new L.LatLng(e.lat, e.lon) :
+            new L.LatLng(e.center.lat, e.center.lon);
 
-	var poi = get_poi(e)
-	// skip this undefined icon
-	if (!poi) {
-	    console.info('Skipping undefined icon: "' + type + '"');
-	    continue;
-	}
+        var poi = get_poi(e)
+        // skip this undefined icon
+        if (!poi) {
+            console.info('Skipping undefined icon: "' + type + '"');
+            continue;
+        }
 
-	var markerIcon  = L.icon({
-	    iconUrl: 'assets/img/icons/' + poi.iconName + '.png',
-	    iconSize: [32, 37],
-	    iconAnchor: [18.5, 35],
-	    popupAnchor: [0, -27]
-	});
-	var marker = L.marker(pos, {
-	    icon: markerIcon,
-	    keyboard: false
-	})
+        var markerIcon  = L.icon({
+            iconUrl: 'assets/img/icons/' + poi.iconName + '.png',
+            iconSize: [32, 37],
+            iconAnchor: [18.5, 35],
+            popupAnchor: [0, -27]
+        });
+        var marker = L.marker(pos, {
+            icon: markerIcon,
+            keyboard: false
+        })
 
-	// show a label next to the icon on mouse hover
-	if (e.tags.name) {
-	    marker.bindLabel(
-		e.tags.name,
-		{direction: 'auto', offset: [27, -32]}
-	    );
-	}
+        // show a label next to the icon on mouse hover
+        if (e.tags.name) {
+            marker.bindLabel(
+                e.tags.name,
+                {direction: 'auto', offset: [27, -32]}
+            );
+        }
 
-	// used to show the expert mode panel side
-	marker._element = e;
-	marker.on('click', function(e) {
-	    var element = e.target._element;
-	    $('#developer > .tags').html(develop_parser(element));
+        // used to show the expert mode panel side
+        marker._element = e;
+        marker.on('click', function(e) {
+            var element = e.target._element;
+            $('#developer > .tags').html(develop_parser(element));
 
-	    var name = element.tags.name ? element.tags.name : element.id;
-	    $('#comments #comment-name').html(name);
-	    // reload disqus thread
-	    DISQUS.reset({
-		reload: true,
-		config: function () {
-		    this.page.identifier = element.id;
-		    this.page.url = 'http://pois.elblogdehumitos.com.ar/#!' + element.id;
-		}
-	    });
-	});
+            var name = element.tags.name ? element.tags.name : element.id;
+            $('#comments #comment-name').html(name);
+            // reload disqus thread
+            DISQUS.reset({
+                reload: true,
+                config: function () {
+                    this.page.identifier = element.id;
+                    this.page.url = 'http://pois.elblogdehumitos.com.ar/#!' + element.id;
+                }
+            });
+        });
 
-	if (poi.tagParser) var markerPopup = poi.tagParser(e);
-	else var markerPopup = generic_poi_parser(e, poi.name);
+        if (poi.tagParser) var markerPopup = poi.tagParser(e);
+        else var markerPopup = generic_poi_parser(e, poi.name);
 
-	marker.bindPopup(markerPopup);
-	marker.addTo(this.instance);
+        marker.bindPopup(markerPopup);
+        marker.addTo(this.instance);
     }
 }
 
 function build_overpass_query() {
     query = '(';
     $('#pois input:checked').each(function(i, element) {
-	query += 'node(BBOX)' + pois[element.dataset.key].query + ';';
-	query += 'way(BBOX)' + pois[element.dataset.key].query + ';';
-	query += 'relation(BBOX)' + pois[element.dataset.key].query + ';';
+        query += 'node(BBOX)' + pois[element.dataset.key].query + ';';
+        query += 'way(BBOX)' + pois[element.dataset.key].query + ';';
+        query += 'relation(BBOX)' + pois[element.dataset.key].query + ';';
     });
     query += ');out center;';
 }
@@ -123,21 +123,21 @@ function show_pois_checkboxes() {
     var content = '';
     content += '<table>';
     for (poi in pois) {
-	if (i % 2 == 0) content += '<tr>';
-	content += '<td>';
-	var checkbox = Mustache.render(
-	    '<div class="poi-checkbox"> \
-		<label> \
-		    <img src="assets/img/icons/{{icon}}.png"></img> \
-		    <input type="checkbox" data-key="{{key}}" onclick="setting_changed()"><span>{{name}}</span> \
-		</label> \
-	    </div>',
-	    {key: poi, name: pois[poi].name, icon: pois[poi].iconName}
-	);
-	content += checkbox;
-	content += '</td>';
-	i++;
-	if (i % 2 == 0) content += '</tr>';
+        if (i % 2 == 0) content += '<tr>';
+        content += '<td>';
+        var checkbox = Mustache.render(
+            '<div class="poi-checkbox"> \
+                <label> \
+                    <img src="assets/img/icons/{{icon}}.png"></img> \
+                    <input type="checkbox" data-key="{{key}}" onclick="setting_changed()"><span>{{name}}</span> \
+                </label> \
+            </div>',
+            {key: poi, name: pois[poi].name, icon: pois[poi].iconName}
+        );
+        content += checkbox;
+        content += '</td>';
+        i++;
+        if (i % 2 == 0) content += '</tr>';
     }
     content += '</table>';
     $('#pois').append(content);
@@ -148,8 +148,8 @@ function show_overpass_layer() {
     $('#develop p.tags').html('');
 
     if (query == '' || query == '();out center;') {
-	console.debug('There is nothing selected to filter by.');
-	return;
+        console.debug('There is nothing selected to filter by.');
+        return;
     }
     var opl = new L.OverPassLayer({
         endpoint: "https://overpass-api.de/api/",
