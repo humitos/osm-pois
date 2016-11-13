3f9a4d77331e0541b2552ac92117c6d6e7fd21f6
Manuel Kaufmann <humitos@gmail.com>
2016-11-13 16:14:41 -0500

    Update fabric commands

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function, unicode_literals
+
 import os
-from fabric.api import run, env, cd
+
+from fabric.api import cd, env, run
 
 # We can then specify host(s) and run the same commands across those systems
 env.user = 'humitos'
-env.hosts = ['mkaufmann.com.ar']
+env.hosts = ['elblogdehumitos.com']
 env.shell = '/bin/bash -l -c'
 env.colorize_errors = True
 env.project = '/home/humitos/apps/osm-pois'
