3499981e0b802dee87087ac6791d48758a84c56a
laureano <laureano@binn.com.ar>
2017-02-21 17:36:09 -0300

    setting up i18n

--- upoi/upoi/settings.py
+++ upoi/upoi/settings.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ar'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Argentina/Buenos_Aires'
 
 USE_I18N = True
 
