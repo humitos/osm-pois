fdd5f5e3753e7a06a0eb2908186d2e53cb833759
Elena-GHub <cabjelen@yahoo.es>
2020-12-28 10:10:49 +0100

    refactor all upoi paths to poi

--- oursite/upoi/settings.py
+++ oursite/upoi/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'upoi.apps.UpoiConfig',
+    'poi.apps.PoiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
