7237b3a0e7a3cf31866120d2ae9bafcf0129fd4b
Elena-GHub <cabjelen@yahoo.es>
2020-12-28 10:39:49 +0100

    refactor oursite project name to upoi

--- upoi/upoi/asgi.py
+++ upoi/upoi/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for oursite project.
+ASGI config for upoi project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oursite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'upoi.settings')
 
 application = get_asgi_application()
