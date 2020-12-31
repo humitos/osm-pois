8a9e29fb00234e797b47f5a52004d8e0afcfa7e0
Elena-GHub <cabjelen@yahoo.es>
2020-12-31 10:06:08 +0100

    changed PointField type to CharField to keep simple coordinates

--- upoi/poi/models/poi.py
+++ upoi/poi/models/poi.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.gis.db import models
+# from django.contrib.gis.db import models
 
 
 class Poi(models.Model):
@@ -8,5 +8,6 @@ class Poi(models.Model):
     phone = models.CharField(max_length=20)
     email = models.CharField(max_length=50)
     website = models.CharField(max_length=150)
-    coordinates = models.PointField()
+    # coordinates = models.PointField()
+    coordinates = models.CharField(max_length=100)
     social_media = models.CharField(max_length=300)
