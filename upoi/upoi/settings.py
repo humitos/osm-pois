77115b212caa4f410f0941eb62839f778e7c4d43
Elena-GHub <60514098+Elena-GHub@users.noreply.github.com>
2021-03-09 20:42:47 +0100

    fixed path for layers.png and layers-2x.png

--- upoi/upoi/settings.py
+++ upoi/upoi/settings.py
@@ -115,7 +115,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, img)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
