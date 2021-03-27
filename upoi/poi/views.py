c8a432231cf2887c7645b6616d8893eb1535fcc2
Elena-GHub <60514098+Elena-GHub@users.noreply.github.com>
2021-03-27 11:09:39 +0100

    show in index.html pois created by admin as a list

--- upoi/poi/views.py
+++ upoi/poi/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.urls import reverse
 from django.views import View
+from .models import Poi
 
 def index(request):
-    return render(request, 'poi/index.html')
+    # return render(request, 'poi/index.html')
+    all_poi_list = Poi.objects.all()
+    context = {'all_poi_list': all_poi_list}
+    return render(request, 'poi/index.html', context)
