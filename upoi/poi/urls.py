35f6a89ea153b24001fef26c318adc4de21c6897
Elena-GHub <60514098+Elena-GHub@users.noreply.github.com>
2021-04-17 13:27:40 +0200

    turned index view in its own class with get_queryset method

--- upoi/poi/urls.py
+++ upoi/poi/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'poi'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
 ]
--- upoi/poi/views.py
+++ upoi/poi/views.py
@@ -1,10 +1,16 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-from django.views import View
+from django.views import generic
 from .models import Poi
 
-def index(request):
-    # return render(request, 'poi/index.html')
-    all_poi_list = Poi.objects.all()
-    context = {'all_poi_list': all_poi_list}
-    return render(request, 'poi/index.html', context)
+
+class IndexView(generic.ListView):
+    template_name = 'poi/index.html'
+    context_object_name = 'all_poi_list'
+        
+    def get_queryset(self):
+        """
+        """      
+        pois = Poi.objects.all()
+        return pois
